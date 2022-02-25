99cd0908c061be21f502ab3cb7bc716b4480754b
etblh <hugo432121@gmail.com>
2022-02-26 00:15:19 +0800

    0002

--- src/b02-3-0002/main.js
+++ src/b02-3-0002/main.js
@@ -2,21 +2,43 @@ let game = (() => {
 
     let element = {
         rods: document.querySelectorAll(".rod"),
-        rolls: document.querySelectorAll(".roll .view")
+        rolls: document.querySelectorAll(".roll .view"),
+        indicator: document.querySelector(".indicator"),
+        right: document.querySelector(".right"),
+        timer: document.query
     }
 
     let pulled = [0,0,0];
+    let pullpx = [0,0,0];
     let pulling = [false, false, false];
-    const pulledpx = [213,131,73];
+    const pullheight = [213,130,73];
+    const target = ['r','e','m','p','c'];
+    let current_idx = 0;
+    const order = [
+        ['e','p','m','c','r'],
+        ['r','c','p','m','e'],
+        ['c','r','p','m','e']
+    ];
+
+    let countdown;
+    let timer;
 
     let start = () => {
+        countdown = setInterval(() => {
+            timer--;
+            
+            element.timer.innerHTML=`00:${timer/10<1?"0":""}${timer}`;
+        },1000);
+    }
 
+    let win = () => {
+        clearInterval(countdown);
+        
     }
 
     return{
         start: start,
         restart: () => {
-            
             start();
         },
         init: () => {
@@ -28,10 +50,39 @@ let game = (() => {
                     setTimeout(() => {
                         ele.classList.remove("pulled");
                         pulling[key] = false;
-                    },1000);
-                    pulled[key] += pulledpx[key];
-                    element.rolls[key].style["background-position"] = "0px " + pulled[key]+"px";
-                    console.log(element.rolls);
+                    },600);
+                    let currentpx = pullpx[key];
+                    pullpx[key] += pullheight[key];
+                    pulled[key]++;
+                    let steps = pullheight[key]/30;
+                    let frame = 0;
+                    let animator = setInterval(() => {
+                        if (frame >= 30){
+                            clearInterval(animator);
+                            return;
+                        }
+                        currentpx+=steps;
+                        element.rolls[key].style["background-position"] = "0px " + currentpx+"px";
+                        frame++;
+                    },300/30);
+
+                    if (target[current_idx] == order[0][pulled[0]%5] &&
+                        target[current_idx] == order[1][pulled[1]%5] &&
+                        target[current_idx] == order[2][pulled[2]%5]){
+                            current_idx++;
+                            element.indicator.className = "element indicator "+target[current_idx];
+                            element.right.classList.add("show");
+                            setTimeout(() => {
+                                element.right.classList.remove("show");
+                            },500);
+                            play_music("sounds/1_正確.mp3");
+                            if (current_idx >= 5){
+                                win();
+                                return;
+                            }
+                    }
+                    console.log(order[0][pulled[0]%5],order[1][pulled[1]%5],order[2][pulled[2]%5])
+                    
                 });
             })
         }
